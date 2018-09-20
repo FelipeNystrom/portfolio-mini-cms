037c28ca34d12d22fb3bb5036706624d42c7b72a
FelipeNystrom <andre.felipe.nystrom@gmail.com>
2018-09-20 13:08:37 +0200

    fix title position

--- src/TitleAnimationOnLoad.js
+++ src/TitleAnimationOnLoad.js
@@ -15,21 +15,23 @@ class TitleAnimation extends Component {
     const { upper, lower } = this.props;
     return (
       <div className={styles.titleWrapper}>
-        <div
-          className={
-            !animate
-              ? styles.upper
-              : styles.upper + ' ' + styles.showBorderBottom
-          }
-        >
-          {upper}
-        </div>
-        <div
-          className={
-            !animate ? styles.lower : styles.lower + ' ' + styles.animateLower
-          }
-        >
-          {lower}
+        <div className={styles.animationWrapper}>
+          <div
+            className={
+              !animate
+                ? styles.upper
+                : styles.upper + ' ' + styles.showBorderBottom
+            }
+          >
+            {upper}
+          </div>
+          <div
+            className={
+              !animate ? styles.lower : styles.lower + ' ' + styles.animateLower
+            }
+          >
+            {lower}
+          </div>
         </div>
       </div>
     );
