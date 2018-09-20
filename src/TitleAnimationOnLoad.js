a19b95e818a6c9836094bfbc00dc457504e4bacc
FelipeNystrom <andre.felipe.nystrom@gmail.com>
2018-09-20 12:49:20 +0200

    add title animation in mobile mode

--- src/TitleAnimationOnLoad.js
+++ src/TitleAnimationOnLoad.js
@@ -2,12 +2,35 @@ import React, { Component } from 'react';
 import styles from './TitleAnimationOnLoad.css';
 
 class TitleAnimation extends Component {
+  state = {
+    animate: false
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ animate: true });
+    }, 0);
+  }
   render() {
+    const { animate } = this.state;
     const { upper, lower } = this.props;
     return (
       <div className={styles.titleWrapper}>
-        <div className={styles.firstTitle}>{upper}</div>
-        <div className={styles.secondTitle}>{lower}</div>
+        <div
+          className={
+            !animate
+              ? styles.upper
+              : styles.upper + ' ' + styles.showBorderBottom
+          }
+        >
+          {upper}
+        </div>
+        <div
+          className={
+            !animate ? styles.lower : styles.lower + ' ' + styles.animateLower
+          }
+        >
+          {lower}
+        </div>
       </div>
     );
   }
