e9ec941d57806e01779699c2d8659bdff466aa59
FelipeNystrom <andre.felipe.nystrom@gmail.com>
2018-10-12 10:36:46 +0200

    add blocking of register route to /register post route

--- passport/authentication.js
+++ passport/authentication.js
@@ -5,7 +5,8 @@ const { uploadImg } = require('../imageStorage');
 const {
   createUser,
   verifyUser,
-  getAllProjectsFromUser
+  getAllProjectsFromUser,
+  isThereAUSer
 } = require('../db/queries');
 
 const tokenForUser = user => {
@@ -26,60 +27,65 @@ const signin = (req, res) => {
 };
 
 const signup = async (req, res, next) => {
-  const cloudinaryResult = await uploadImg(req.file.path);
-  const profilePic = await cloudinaryResult.secure_url;
+  const isThereAUser = await userExist();
+  if (isThereAUser !== null) {
+    res.sendStatus(401);
+  } else {
+    const cloudinaryResult = await uploadImg(req.file.path);
+    const profilePic = await cloudinaryResult.secure_url;
 
-  const { username, email, password, firstname, lastname } = req.body;
-  const saltRounds = 12;
-  if (
-    !username ||
-    !password ||
-    !email ||
-    !firstname ||
-    !lastname ||
-    !profilePic
-  ) {
-    res.status(422).send({
-      error:
-        'You must provide an email, username, firstname, lastname, password and a profile picture.'
+    const { username, email, password, firstname, lastname } = req.body;
+    const saltRounds = 12;
+    if (
+      !username ||
+      !password ||
+      !email ||
+      !firstname ||
+      !lastname ||
+      !profilePic
+    ) {
+      res.status(422).send({
+        error:
+          'You must provide an email, username, firstname, lastname, password and a profile picture.'
+      });
+    }
+    verifyUser(username).then(result => {
+      if (result === null) {
+        // see if a user with the given email exists.
+        bcrypt
+          .hash(password, saltRounds)
+          .then(hash => {
+            return createUser(
+              username,
+              firstname,
+              lastname,
+              email,
+              hash,
+              profilePic
+            )
+              .then(newUser => {
+                console.log(newUser);
+                res.send({
+                  token: tokenForUser(newUser),
+                  user: newUser
+                });
+              })
+              .catch(err => {
+                console.log(err);
+                res
+                  .status(400)
+                  .send({ message: 'Error saving user to database.' });
+              });
+          })
+          .catch(err => {
+            console.log(err);
+            return next(err);
+          });
+      } else {
+        res.status(403).send({ message: 'user already exists' });
+      }
     });
   }
-  verifyUser(username).then(result => {
-    if (result === null) {
-      // see if a user with the given email exists.
-      bcrypt
-        .hash(password, saltRounds)
-        .then(hash => {
-          return createUser(
-            username,
-            firstname,
-            lastname,
-            email,
-            hash,
-            profilePic
-          )
-            .then(newUser => {
-              console.log(newUser);
-              res.send({
-                token: tokenForUser(newUser),
-                user: newUser
-              });
-            })
-            .catch(err => {
-              console.log(err);
-              res
-                .status(400)
-                .send({ message: 'Error saving user to database.' });
-            });
-        })
-        .catch(err => {
-          console.log(err);
-          return next(err);
-        });
-    } else {
-      res.status(403).send({ message: 'user already exists' });
-    }
-  });
 };
 
 module.exports = { signup, signin };
