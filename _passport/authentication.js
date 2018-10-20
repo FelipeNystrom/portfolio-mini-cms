3adf916b39d73a76a65dba28d999d291dc157bfd
FelipeNystrom <andre.felipe.nystrom@gmail.com>
2018-10-20 07:58:21 +0200

    change from bcrypt to bcryptjs

--- _passport/authentication.js
+++ _passport/authentication.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jwt-simple');
 const config = require('../_vars');
 const { uploadImg } = require('../_imageStorage');
