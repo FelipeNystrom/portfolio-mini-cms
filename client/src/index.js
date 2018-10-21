b5a2d63a78cbdd240ee4c16c743e5a766eac4c54
FelipeNystrom <andre.felipe.nystrom@gmail.com>
2018-10-21 09:35:39 +0200

    fix again

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import configureStore from './configureStore';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import { loadProjects } from './_actions/projectAction';
 import auth from './_actions/userAction';
 import './_bootstrapLibrary';
