6a64abf0ecc945f085260955eb81c8af2453533b
FelipeNystrom <andre.felipe.nystrom@gmail.com>
2018-10-21 08:07:31 +0200

    rename server.js to index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const db = require('./_db');
+const mountRoutes = require('./_routes');
+const path = require('path');
+const server = express();
+const passport = require('passport');
+const cors = require('cors');
+const _port = process.env.PORT || 7000;
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(passport.initialize());
+server.use(cors());
+require('./_passport')(passport);
+require('dotenv').config();
+
+if (process.env.NODE_ENV === 'production') {
+  server.use(express.static(path.join(__dirname, 'client/build')));
+  // Anything that doesn't match the above, send back index.html
+  server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
+} else {
+  server.use(express.static(path.join(__dirname, 'public')));
+}
+
+// use mounted routes
+mountRoutes(server);
+
+server.listen(_port, () => {
+  console.log(`server running at port: ${_port}`);
+});
+
+server.on('close', () => {
+  console.log('server connection closed');
+  db.pool.end(() => {
+    console.log('database connection closed');
+  });
+});
